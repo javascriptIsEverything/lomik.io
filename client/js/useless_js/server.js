013f6eaca16871ce858c4b43617bbda6a7bc912a
javascriptIsEverything <davitjan99@gmail.com>
2020-01-11 22:48:11 -0800

    Deleting usless folders

--- client/js/useless_js/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-const index = fs.readFileSync('index.html');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
-}).listen(9615);
