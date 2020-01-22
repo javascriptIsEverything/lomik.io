97a9662b8a9669965db70dae3e29f67292ad5d6b
javascriptIsEverything <davitjan99@gmail.com>
2020-01-21 19:59:22 -0800

    Package.json dependencies

--- entities/index.js
+++ entities/index.js
@@ -3,9 +3,6 @@ let checkPlayers = require('./players');
 let checkCells = require('./cells');
 global.Castle = require('./castle');
 
-funciton hello () {
-    // await 
-}
 module.exports = {
     checkEnemies,
     checkPlayers,
